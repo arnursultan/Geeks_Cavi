f84790023112a0f06fbd73dcc8fc6f258afce89f
Nursultan Zhanybekov <121675952+arnursultan@users.noreply.github.com>
2023-05-12 03:41:54 +0600

    Welcome

--- core/settings.py
+++ core/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = '/var/www/rycc/static'
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR.joinpath('media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
